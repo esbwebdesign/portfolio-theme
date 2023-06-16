63cc46442416f878501160fe4849b9d7ecdac6a6
esbwebdesign <31042473+esbwebdesign@users.noreply.github.com>
2023-06-16 03:03:00 -0500

    Sidebar Tag Cloud
    feat: Finish sidebar tag cloud

--- sidebar.php
+++ sidebar.php
@@ -10,8 +10,11 @@
 
 declare(strict_types=1);
 
-$base_indent = 8;
+$base_indent = 9;
+$n = "\n";
+$t = "\t";
 
+// Set up walker to handle category list
 $cat_list = wp_list_categories([
 	'echo' => false,
 	'show_count' => true,
@@ -21,20 +24,38 @@ $cat_list = wp_list_categories([
 	'base_indent' => $base_indent + 1
 ]);
 
+// Set up tag array
+$tag_array = get_tags();
+$tag_output = '';
+// Create a set of links from the tags
+foreach ( $tag_array as $tag ) {
+	// Add a space between links
+	if ( strlen($tag_output) !== 0 ) {
+		$tag_output .= ' ';
+	}
+	$tag_link = get_site_url() . '/tag/' . $tag->slug;
+	$a = '<a href="' . $tag_link . '" class="no-underline link-light">' . $tag->name . '</a>';
+	$tag_output .= $n . str_repeat($t, $base_indent + 1) . '<span class="badge bg-secondary">' . $a . '</span>';
+}
+$tag_output .= $n . str_repeat($t, $base_indent);
+
 if ( $cat_list !== false ) {
 ?>
-								<h5>Categories</h5>
-								<ul class="list-unstyled"><?php echo $cat_list . "\n"; ?>
-								</ul>
+								<div class="mb-4">
+									<h5>Categories</h5>
+									<ul class="list-unstyled"><?php echo $cat_list . "\n"; ?>
+									</ul>
+								</div>
 <?php
 }
 
-$tag_array = get_tags();
 if ( !is_wp_error($tag_array) && count($tag_array) > 0 ) {
 ?>
-								<h5>Tags</h5>
+								<div class="mb-4">
+									<h5>Tags</h5>
+									<div><?php echo $tag_output; ?></div>
+								</div>
 <?php
-	echo var_dump($tag_array);
 }
 
 ?>
